d1d6e231efff9d97d58b51cdf489ee4773ae347b
Benfany Aditia <56857771+benfany505@users.noreply.github.com>
2021-09-12 15:23:16 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,44 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 // Deklarasi Variable dan Konstanta
 String wifiSSID = "smartbuilding_wifi";
 String wifiPassword = "smartbuilding@2020";
+String mqttBroker = "test.mosquitto.org";
+WiFiClient client;
+PubSubClient mqtt(client);
 
 // Deklarasi Fungsi
 void connectWifi();
-
+void connect_mqtt();
 void setup()
 {
   Serial.begin(9600);
   connectWifi();
+  mqtt.setServer(mqttBroker.c_str(), 1883);
 }
 
 void loop()
 {
+  if (!mqtt.connected())
+  {
+    connect_mqtt();
+    Serial.println("MQTT Connected");
+  }
+  mqtt.loop();
+}
 
-  Serial.println("test");
-  delay(1000);
+void connect_mqtt()
+{
+  while (!mqtt.connected())
+  {
+    Serial.println("Connecting MQTT...");
+    if (mqtt.connect("esp32"))
+    {
+      mqtt.subscribe("esp32/temphum");
+    }
+  }
 }
 
 void connectWifi()
